d04b4af4978a992157cbd74b9d2fb256e07e6344
ka-js-23 <130503058+ka-js-23@users.noreply.github.com>
2023-04-13 17:01:06 +0100

    List Group

---
+++ src/MoviesApp.jsx
@@ -0,0 +1,9 @@
+import Welcome from './Welcome';
+import ListGroup from './components/ListGroup';
+
+function App () {
+    return <div><Welcome /></div>
+    <div><ListGroup /></div>
+}
+
+export default App;
---
+++ src/Welcome.jsx
@@ -0,0 +1,6 @@
+function Welcome () {
+    const name = 'Katie';
+    return <h1>Movies for {name}</h1>;
+}
+
+export default Welcome;
