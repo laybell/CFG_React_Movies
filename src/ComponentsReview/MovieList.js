cd6bff0191aa7a3927f6adcbb9f0af1f0efe2aa3
ka-js-23 <130503058+ka-js-23@users.noreply.github.com>
2023-04-22 23:09:58 +0100

    Add to Watchlist button on search results

--- src/ComponentsReview/MovieList.js
+++ src/ComponentsReview/MovieList.js
@@ -10,9 +10,23 @@ function MovieList(props){
 			{props.movies.map((movie, index) => (
 				<div >
 					<img src={movie.Poster} alt='movie'></img>
+					<div className="info">
+						<div className="header">
+							<h3 className="title">{movie.title}</h3>	
+							</div>
+					</div>
+					<div className="controls">
+		<button className="btn">Add to Watchlist</button>
+	</div>
+
 				</div>
+				
 			))}
+
+			
+			
 		</>
+		
 	);
 };
 
